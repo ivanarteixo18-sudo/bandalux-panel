787f83ce0fb3eb373f314278c894386ab28cd831
ivanarteixo18-sudo <ivanarteixo18@gmail.com>
2026-09-08 00:33:51 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const VERSION="bandalux-v18";
+
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request, {cache:"no-store"})
+      .catch(() => caches.match(event.request))
+  );
+});
